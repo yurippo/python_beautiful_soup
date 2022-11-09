1142060b409ff124d34934d8bd80931389ca9777
Yuri Prata <yuri.prata12@gmail.com>
2022-11-09 13:49:20 -0800

    Yoga Commit  2022-11-09 ;)

---
+++ yoda.py
@@ -0,0 +1,11 @@
+def yoda(a,b,c):
+ return c,a,b
+
+print(''.join(yoda('Python','Is','Fun'))+ '!')
+
+users = ['beatle','egg man']
+
+while (user:= input()) in users:
+    print('in use')
+print('I am the ', user)
+
